769dd97eb28f1cc814695402b18aa1c6cfe2cd7b
Alex Bigelow <alex.r.bigelow@gmail.com>
2018-04-03 07:16:25 -0600

    Forgot to include flattened files

---
+++ src/DocHandler.js
@@ -0,0 +1,133 @@
+import mime from 'mime-types';
+import datalib from 'datalib';
+import mureInteractivityRunnerText from './DocHandler/mureInteractivityRunner.text.js'; // eslint-disable-line no-unused-vars
+import defaultSvgContentTemplate from './DocHandler/default.text.svg';
+import minimumSvgContent from './DocHandler/minimum.text.svg';
+
+// sneakily embed the interactivity-running script
+const defaultSvgContent = defaultSvgContentTemplate.replace(/\${mureInteractivityRunnerText}/, mureInteractivityRunnerText);
+
+class DocHandler {
+  constructor (mure) {
+    this.mure = mure;
+    this.keyNames = {};
+    this.datalibFormats = ['json', 'csv', 'tsv', 'dsv', 'topojson', 'treejson'];
+    this.defaultSvgContent = this.parseXml(defaultSvgContent);
+    this.minimumSvgContent = this.parseXml(minimumSvgContent);
+  }
+  async parse (text, { format = {}, mimeType } = {}) {
+    if (mimeType && (!format || !format.type)) {
+      format.type = mime.extension(mimeType);
+    }
+    let contents;
+    if (format.type) {
+      format.type = format.type.toLowerCase();
+      if (this.datalibFormats.indexOf(format.type) !== -1) {
+        contents = datalib.read(text, format);
+      } else if (format.type === 'xml') {
+        contents = this.parseXml(text, format);
+      }
+    }
+    if (!contents.contents) {
+      contents = { contents: contents };
+    }
+    return contents;
+  }
+  parseXml (text, { format = {} } = {}) {
+    return { todo: true };
+  }
+  formatDoc (doc) {
+    // TODO
+    return 'todo';
+  }
+  isValidId (docId) {
+    if (docId[0].toLowerCase() !== docId[0]) {
+      return false;
+    }
+    let parts = docId.split(';');
+    if (parts.length !== 2) {
+      return false;
+    }
+    return !!mime.extension(parts[0]);
+  }
+  async standardize (doc, { purgeArrays = false } = {}) {
+    if (!doc._id || !this.isValidId(doc._id)) {
+      if (!doc.mimeType && !doc.filename) {
+        // Without an id, filename, or mimeType, just assume it's application/json
+        doc.mimeType = 'application/json';
+      }
+      doc.mimeType = doc.mimeType.toLowerCase();
+      if (!doc.filename) {
+        if (doc._id) {
+          // We were given an invalid id; use it as the filename instead
+          doc.filename = doc._id;
+        } else {
+          // Without anything to go on, use "Untitled 1", etc
+          let existingUntitleds = await this.mure.db.allDocs({
+            startkey: doc.mimeType + ';Untitled ',
+            endkey: doc.mimeType + ';Untitled \uffff'
+          });
+          let minIndex = existingUntitleds.rows.reduce((minIndex, uDoc) => {
+            let index = /Untitled (\d+)/g.exec(uDoc._id);
+            index = index ? index[1] || Infinity : Infinity;
+            return index < minIndex ? index : minIndex;
+          }, Infinity);
+          minIndex = isFinite(minIndex) ? minIndex + 1 : 1;
+          doc.filename = 'Untitled ' + minIndex;
+        }
+      }
+      if (!doc.mimeType) {
+        // We were given a bit of info with the filename / bad _id;
+        // try to infer the mimeType from that (again use application/json
+        // if that fails)
+        doc.mimeType = mime.lookup(doc.filename) || 'application/json';
+      }
+      doc._id = doc.mimeType + ';' + doc.filename;
+    }
+    if (doc._id[0] === '_' || doc._id[0] === '$') {
+      throw new Error('Document _ids may not start with ' + doc._id[0] + ': ' + doc._id);
+    }
+    if (!doc.mimeType) {
+      doc.mimeType = doc._id.split(';')[0];
+    }
+    if (!mime.extension(doc.mimeType)) {
+      this.mure.warn('Unknown mimeType: ' + doc.mimeType);
+    }
+    if (!doc.filename) {
+      doc.filename = doc._id.split(';')[1];
+    }
+    if (!doc.contents) {
+      doc.contents = {};
+    }
+    if (purgeArrays) {
+      [doc.contents, doc.purgedArrays] = this.purgeArrays(doc.contents);
+    }
+    return doc;
+  }
+  purgeArrays (obj) {
+    if (typeof obj !== 'object') {
+      return [obj, false];
+    }
+    let foundArray = false;
+    if (obj instanceof Array) {
+      let temp = {};
+      obj.forEach((element, index) => {
+        temp[index] = element;
+      });
+      obj = temp;
+      foundArray = true;
+    }
+    Object.keys(obj).forEach(key => {
+      let foundChildArray, childObj;
+      [childObj, foundChildArray] = this.purgeArrays(obj[key]);
+      obj[key] = childObj;
+      foundArray = foundArray || foundChildArray;
+    });
+    return [obj, foundArray];
+  }
+  restoreArrays (obj) {
+    // todo
+  }
+}
+
+export default DocHandler;
---
+++ src/Mure.js
@@ -0,0 +1,233 @@
+import mime from 'mime-types';
+import jsonPath from 'jsonpath';
+import { Model } from 'uki';
+import Selection from './Selection.js';
+import DocHandler from './DocHandler.js';
+
+class Mure extends Model {
+  constructor (PouchDB, d3, d3n) {
+    super();
+    this.PouchDB = PouchDB; // could be pouchdb-node or pouchdb-browser
+    this.d3 = d3; // for Node.js, this will be from d3-node, not the regular one
+
+    if (d3n) {
+      // to run tests, we also need access to the d3-node wrapper (we don't
+      // import it directly into the tests to make sure that the namespace
+      // addition below works)
+      this.d3n = d3n;
+      this.window = this.d3n.window;
+    } else {
+      this.window = window;
+    }
+
+    // The namespace string for our custom XML
+    this.NSString = 'http://mure-apps.github.io';
+    this.d3.namespaces.mure = this.NSString;
+
+    this.docHandler = new DocHandler(this);
+
+    // Create / load the local database of files
+    this.getOrInitDb();
+
+    // in the absence of a custom dialogs, just use window.alert,
+    // window.confirm, window.prompt, console.warn, and console.log:
+    this.alert = (message) => {
+      return new Promise((resolve, reject) => {
+        this.window.alert(message);
+        resolve(true);
+      });
+    };
+    this.confirm = (message) => {
+      return new Promise((resolve, reject) => {
+        resolve(this.window.confirm(message));
+      });
+    };
+    this.prompt = (message, defaultValue) => {
+      return new Promise((resolve, reject) => {
+        resolve(this.window.prompt(message, defaultValue));
+      });
+    };
+    this.warn = function () {
+      console.warn(...arguments);
+    };
+    this.log = function () {
+      console.log(...arguments);
+    };
+  }
+  customizeAlertDialog (showDialogFunction) {
+    this.alert = showDialogFunction;
+  }
+  customizeConfirmDialog (showDialogFunction) {
+    this.confirm = showDialogFunction;
+  }
+  customizePromptDialog (showDialogFunction) {
+    this.prompt = showDialogFunction;
+  }
+  openApp (appName, newTab) {
+    if (newTab) {
+      this.window.open('/' + appName, '_blank');
+    } else {
+      this.window.location.pathname = '/' + appName;
+    }
+  }
+  async getOrInitDb () {
+    this.db = new this.PouchDB('mure');
+    let status = { synced: false };
+    let couchDbUrl = this.window.localStorage.getItem('couchDbUrl');
+    if (couchDbUrl) {
+      let couchDb = new this.PouchDB(couchDbUrl, {skip_setup: true});
+      status.synced = !!(await this.db.sync(couchDb, {live: true, retry: true})
+        .catch(err => {
+          this.alert('Error syncing with ' + couchDbUrl + ': ' +
+            err.message);
+          return false;
+        }));
+    }
+    status.indexed = !!(await this.db.createIndex({
+      index: {
+        fields: ['filename']
+      }
+    }).catch(() => false));
+    status.selectionAdded = !!(await this.db.put({
+      _id: '$currentSelector',
+      selector: null
+    }).catch(() => false));
+    this.db.changes({
+      since: 'now',
+      live: true,
+      selector: { _id: {'$gt': '$', '$lt': '$\uffff'} }
+    }).on('change', change => {
+      if (change.id === '$currentSelector') {
+        this.trigger('selectionChange', change.selector);
+      }
+    }).on('error', err => {
+      this.warn(err);
+    });
+    return status;
+  }
+  /**
+   * A wrapper around PouchDB.get() that ensures that the first matched
+   * document exists (optionally creates an empty document when it doesn't), and
+   * that it conforms to the specifications outlined in documentation/schema.md
+   * @param  {Object|string}  [docQuery]
+   * The `selector` component of a Mango query, or, if a string, the precise
+   * document _id
+   * @param  {{boolean}}  [init=true]
+   * If true (default), the document will be created (but not saved) if it does
+   * not exist. If false, the returned Promise will resolve to null
+   * @return {Promise}
+   * Resolves the document
+   */
+  async getDoc (docQuery, { init = true } = {}) {
+    let doc;
+    if (!docQuery) {
+      doc = {};
+    } else {
+      if (typeof docQuery === 'string') {
+        if (docQuery[0] === '@') {
+          docQuery = docQuery.slice(1);
+        } else {
+          docQuery = { '_id': docQuery };
+        }
+      }
+      let matchingDocs = await this.db.find({ selector: docQuery, limit: 1 });
+      if (matchingDocs.docs.length === 0) {
+        if (init) {
+          // If missing, use the docQuery itself as the template for a new doc
+          doc = docQuery;
+        } else {
+          return null;
+        }
+      } else {
+        doc = matchingDocs.docs[0];
+      }
+    }
+    return this.docHandler.standardize(doc);
+  }
+  /**
+   * Downloads a given file, optionally specifying a particular format
+   * @param  {Object|string}  docQuery
+   * The `selector` component of a Mango query, or, if a string, the precise
+   * document _id
+   * @param  {{string|null}}  [mimeType=null]
+   * Overrides the document's mimeType in formatting the download
+   * @return {Promise}
+   * Resolves as `true` once the download is initiated
+   */
+  async downloadDoc (docQuery, { mimeType = null } = {}) {
+    return this.getDoc(docQuery)
+      .then(doc => {
+        mimeType = mimeType || doc.mimeType;
+        let contents = this.docHandler.formatDoc(doc, { mimeType });
+
+        // create a fake link to initiate the download
+        let a = document.createElement('a');
+        a.style = 'display:none';
+        let url = this.window.URL.createObjectURL(new window.Blob([contents], { type: mimeType }));
+        a.href = url;
+        a.download = doc._id;
+        document.body.appendChild(a);
+        a.click();
+        this.window.URL.revokeObjectURL(url);
+        a.parentNode.removeChild(a);
+
+        return true;
+      });
+  }
+  async uploadFileObj (fileObj, { encoding = mime.charset(fileObj.type) } = {}) {
+    let string = await new Promise((resolve, reject) => {
+      let reader = new window.FileReader();
+      reader.onload = () => {
+        resolve(reader.result);
+      };
+      reader.readAsText(fileObj, encoding);
+    });
+    return this.uploadString(fileObj.name, fileObj.type, string);
+  }
+  async uploadString (filename, mimeType, string) {
+    let doc = await this.docHandler.parse(string, { mimeType });
+    return this.uploadDoc(filename, mimeType, doc);
+  }
+  async uploadDoc (filename, mimeType, doc) {
+    doc.filename = filename || doc.filename;
+    doc.mimeType = mimeType || doc.mimeType;
+    doc = await this.docHandler.standardize(doc, { purgeArrays: true });
+    return this.db.put(doc);
+  }
+  async saveDoc (doc) {
+    return this.db.put(await this.docHandler.standardize(doc));
+  }
+  async deleteDoc (docQuery) {
+    let doc = await this.getDoc(docQuery);
+    return this.db.put({
+      _id: doc._id,
+      _rev: doc._rev,
+      _deleted: true
+    });
+  }
+  pathsToSelector (paths = [[Selection.DEFAULT_DOC_QUERY]]) {
+    throw new Error('unimplemented');
+  }
+  pathToSelector (path = [Selection.DEFAULT_DOC_QUERY]) {
+    let docQuery = path[0];
+    let objQuery = path.slice(1);
+    objQuery = objQuery.length > 0 ? jsonPath.stringify(objQuery) : '';
+    return '@' + docQuery + objQuery;
+  }
+  selectDoc (docId) {
+    return this.select('@{"_id":"' + docId + '"}');
+  }
+  select (selector) {
+    return new Selection(this, selector, { selectSingle: true });
+  }
+  selectAll (selector) {
+    return new Selection(this, selector);
+  }
+  async setSelector (selector) {
+    let currentSelector = await this.db.get('$currentSelector');
+    currentSelector.selector = selector;
+    return this.db.put(currentSelector);
+  }
+}
+
+export default Mure;
