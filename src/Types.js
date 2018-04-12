eba7231521ed2440d24d94344640d116d57fb166
Alex Bigelow <alex.r.bigelow@gmail.com>
2018-04-12 15:50:18 -0600

    Forgot to add files for type inference in earlier commit

---
+++ src/Types.js
@@ -0,0 +1,13 @@
+import createEnum from './createEnum.js';
+export default createEnum([
+  'boolean',
+  'number',
+  'string',
+  'date',
+  'undefined',
+  'null',
+  'reference',
+  'container',
+  'document',
+  'root'
+]);
