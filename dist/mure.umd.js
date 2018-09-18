a566daac6f8ff83b64667577c6381bbb438a0c5d
Alex Bigelow <alex.r.bigelow@gmail.com>
2018-09-18 11:17:51 -0600

    Update package version

--- dist/mure.umd.js
+++ dist/mure.umd.js
@@ -20199,7 +20199,7 @@
 	}
 
 	var name$1 = "mure";
-	var version$1 = "0.5.8";
+	var version$1 = "0.5.9";
 	var description$1 = "A library for flexible graph reshaping";
 	var main$1 = "dist/mure.cjs.js";
 	var module$1 = "dist/mure.esm.js";
